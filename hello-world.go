042a0d658e852aaceca1f94730957bc778bfa63e
Daniel Black <daniel.black@thit.com>
2017-12-27 14:43:09 -0600

    Function call example

--- hello-world.go
+++ hello-world.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -9,12 +10,14 @@ func main() {
 	name := "Daniel"
 	course := "Docker Deep Dive"
 
-	fmt.Println("\nHi", name, "you're currently watching",
-		course)
+	fmt.Println(converter(name, course))
 
-	changeCourse(&course)
+	//fmt.Println("\nHi", name, "you're currently watching",
+	//	course)
 
-	fmt.Println("\nYou are now watching course", course)
+	//changeCourse(&course)
+
+	//fmt.Println("\nYou are now watching course", course)
 }
 
 func changeCourse(course *string) string {
@@ -23,4 +26,11 @@ func changeCourse(course *string) string {
 	fmt.Println("\nTrying to change your course to", *course)
 
 	return *course
+}
+
+func converter(s1, s2 string) (str1, str2 string) {
+	s1 = strings.Title(s1)
+	s2 = strings.ToUpper(s2)
+
+	return s1, s2
 }
